49b1be7b955e7c7d2a4e7462139b87c26d4a95f0
makiptv <nasinikola15@gmail.com>
2025-03-20 11:16:32 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="bg" >
+
+<script async src="../../../securepubads.g.doubleclick.net/tag/js/f.txt" type="8e2fb4c017daceba20cbee0e-text/javascript"></script><meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+<meta name="robots" content="noindex">
+<meta name="googlebot" content="noindex">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<link rel="stylesheet" href="http://rayuvtsi-stb.site/guideV1/parts/bootstrap.min9f72.css?refresh2" />
+<link rel="stylesheet" href="http://rayuvtsi-stb.site/guideV1/parts/reklamav26a13.css?refresh1" />
+<link rel="stylesheet" href="http://rayuvtsi-stb.site/guideV1/parts/app-launcherd2f5.css?refresh735" />
+<link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
+<style>
+
+        body {
+
+            background-color: black; /* Set the background color to black */
+
+            text-align: center;
+
+            margin-left: auto;
+
+            margin-right: auto;
+
+            font-family: 'Oswald';
+
+            font-size: 20px;
+
+        }
+
+    </style>
+
+</head>
+
+</div>
+
+<body>
+
+<a href="http://rayuvtsi-stb.site/ott/index-1.php" class="btn btn-dark menu">
+
+<img src="https://www.fg-a.com/flags/animated-albanian-flag-2.gif">
+
+<div class="menu-titull">ALBANIA</div>
+
+<a href="http://rayuvtsi-stb.site/ott/index-5.php" class="btn btn-dark menu">
+
+<img src="https://www.fg-a.com/flags/animated-albanian-flag-2.gif">
+
+<div class="menu-titull">NIMI TV</div>
+
+
+</a><a href="http://rayuvtsi-stb.site/ott/index-2.php" class="btn btn-dark menu">
+
+<img src="https://www.crossed-flag-pins.com/animated-flag-gif/gifs/Bulgaria_240-animated-flag-gifs.gif">
+
+<div class="menu-titull">BULGARIA</div>
+
+</a><a href="http://rayuvtsi-stb.site/ott/index-3.php" class="btn btn-dark menu">
+
+<img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjmN19a00va64sbVyi0DmHg6iK4MFuYWkSxntH2jC8FGfT3j6kBRFq5xA1XrFxHtvgpfNiJ0TUGJJ5P_hlCx8AdtECX55X0160nf4UyJBI5PhuIXBIrMMCjShzuZc8IB5qbswbm0oKtfBE/s0/Flag_of_Macedonia.gif">
+
+<div class="menu-titull">MAKEDONIA</div>
+
+<a href="http://rayuvtsi-stb.site/ott/index-4.php" class="btn btn-dark menu">
+
+<img src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExc2c0OTB4ZWZ3c3RldnIxMjh2aTdqZWZsaDBjenloNTl1YTVnNm5nZSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/UVNJ7x7Ul3wIbjIhkA/giphy.gif">
+
+<div class="menu-titull">ITALIA</div>
+
+</body>
+
+
+
+
+</html>
